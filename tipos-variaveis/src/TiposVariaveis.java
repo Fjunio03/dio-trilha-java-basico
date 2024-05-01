c69f75c91f6b01190f5d4eb280c486807db9a586
Flavio Junio <juniorflavio540@yahoo.com>
2024-05-01 10:58:44 -0300

    final numbers

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // TiposEVariaveis.java
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+        numero = 0;
+
+        final double VALOR_DE_PI = 3.14;
+        
     }
 }
